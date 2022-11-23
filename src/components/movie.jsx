8638ad0ee55833ca4cd55a7d5cb32d1d7102bb4a
CodingIsaac <isaacijuo@gmail.com>
2022-11-23 13:18:21 +0000

    Movies Behaviour Final Commit

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
@@ -7,12 +7,17 @@ class Movies extends Component {
   };
 
   handleDelete = (movie) => {
-    const movies = this.state.movies.filter(m => m._id != movie._id);
-    this.setState({ movies })
-
+    const movies = this.state.movies.filter((m) => m._id != movie._id);
+    this.setState({ movies });
   };
   render() {
+
+    const { length: moviesCount } = this.state.movies; 
+    if (moviesCount === 0)
+        return<p>We have no movies at the moment</p>
     return (
+        <Fragment>
+        <p>Showing {moviesCount} movies in our Show Room</p>
       <table className="table">
         <thead>
           <tr>
@@ -20,22 +25,28 @@ class Movies extends Component {
             <th>Genre</th>
             <th>Number</th>
             <th>Ratings</th>
-           
           </tr>
         </thead>
         <tbody>
-        {this.state.movies.map(movie => (
-            <tr key = {movie._id}>
+          {this.state.movies.map((movie) => (
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
-              <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Remove</button></td>
-              
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Remove
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
+      </Fragment>
     );
   }
 }
