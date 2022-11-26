12a1e81f83a9ab576c239aff95d06f1786fe3267
CodingIsaac <isaacijuo@gmail.com>
2022-11-26 13:44:18 +0000

    Proptypes Component Commit

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import _ from "lodash";
+import PropTypes from 'prop-types';
 
 const Pagination = (props) => {
   const { itemsCount, pageSize, currentPage, onPageChange } = props;
@@ -29,5 +30,13 @@ const Pagination = (props) => {
     </Fragment>
   );
 };
+
+Pagination.propTypes= {
+  itemsCount: PropTypes.number.isRequired,
+   pageSize: PropTypes.number.isRequired,
+    currentPage: PropTypes.number.isRequired,
+     onPageChange: PropTypes.func.isRequired
+
+}
 
 export default Pagination;
