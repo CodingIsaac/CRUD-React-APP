d8d01d90a886185cfb0cf7a51b7a7529c23806d8
CodingIsaac <isaacijuo@gmail.com>
2022-11-27 14:34:24 +0000

    React-Router Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Movies from './components/movie';
 import './App.css';
+import { Route } from 'react-router-dom'
 
 function App() {
   return (
     <main className='container'>
-      <Movies />
+    <Route path='/movies' components={Movies}></Route>
+    <Route path='/customers' components={Customers}></Route>
+    <Route path='/rentals' components={Rentals}></Route>
+    <Route path='/not-found' components={NotFOund}></Route>
+
+
       </main>
   );
 }
